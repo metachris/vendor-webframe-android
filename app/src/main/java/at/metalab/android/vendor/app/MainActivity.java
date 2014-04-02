72db9021cb9832557d3cf59717fc2c4af5b7fe6d
Chris Hager <chris@metachris.org>
2014-04-02 22:17:36 +0200

    webview added

--- app/src/main/java/at/metalab/android/vendor/app/MainActivity.java
+++ app/src/main/java/at/metalab/android/vendor/app/MainActivity.java
@@ -2,11 +2,18 @@ package at.metalab.android.vendor.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
+/**
+ * Created by Chris Hager <chris@linuxuser.at> on 02/04/14.
+ */
 public class MainActivity extends Activity {
     private static final String TAG = "";
+    private String URL = "http://www.hackerspaceshop.com/automat/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +22,19 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_IMMERSIVE);
 
         setContentView(R.layout.activity_fullscreen);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl(URL);
     }
 
 }
