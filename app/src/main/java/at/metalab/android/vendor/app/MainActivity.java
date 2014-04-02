2ed62d8153acd445e3a4335c2d969600a161d3ac
Chris Hager <chris@metachris.org>
2014-04-02 22:48:52 +0200

    almost there

--- app/src/main/java/at/metalab/android/vendor/app/MainActivity.java
+++ app/src/main/java/at/metalab/android/vendor/app/MainActivity.java
@@ -2,18 +2,26 @@ package at.metalab.android.vendor.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.os.Handler;
 
 /**
  * Created by Chris Hager <chris@linuxuser.at> on 02/04/14.
  */
 public class MainActivity extends Activity {
-    private static final String TAG = "";
-    private String URL = "http://www.hackerspaceshop.com/automat/";
+    private static final String TAG = "VendorMainApp";
+    private static final String URL = "http://www.hackerspaceshop.com/automat/";
+
+    // Whether to enable WakeLock (to keep screen from dimming)
+    private static final boolean keepScreenOn = true;
+    private static final boolean useWakeLock = false;
+
+    Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +30,28 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_IMMERSIVE);
 
         setContentView(R.layout.activity_fullscreen);
+
+        hideSystemUi();
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if (visibility == 0) {
+                    mHandler.postDelayed(mHideRunnable, 2000);
+                }
+            }
+        });
+
+        if (keepScreenOn) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+
+        if (useWakeLock) {
+            PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+            PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+            wakeLock.acquire();
+        }
 
         WebView webView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
@@ -37,4 +59,19 @@ public class MainActivity extends Activity {
         webView.loadUrl(URL);
     }
 
+    private void hideSystemUi() {
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hideSystemUi();
+        }
+    };
 }
