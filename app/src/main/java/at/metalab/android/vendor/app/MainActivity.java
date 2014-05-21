e1c5e5b6a97dc125815b9ab75d051746812513d5
Chris Hager <chris@metachris.org>
2014-05-22 00:22:01 +0200

    working v1

--- app/src/main/java/at/metalab/android/vendor/app/MainActivity.java
+++ app/src/main/java/at/metalab/android/vendor/app/MainActivity.java
@@ -1,14 +1,20 @@
 package at.metalab.android.vendor.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.text.Editable;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.os.Handler;
+import android.widget.EditText;
 
 /**
  * Created by Chris Hager <chris@linuxuser.at> on 02/04/14.
@@ -18,10 +24,14 @@ public class MainActivity extends Activity {
     private static final String URL = "http://www.hackerspaceshop.com/automat/";
 
     // Whether to enable WakeLock (to keep screen from dimming)
-    private static final boolean keepScreenOn = true;
+    private static final boolean keepScreenOn = false;
     private static final boolean useWakeLock = false;
+    private static final boolean hideSystemUI = true;
 
     Handler mHandler = new Handler();
+
+    private long lastTouchMs = 0;
+    private int touchCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +43,17 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_fullscreen);
 
-        hideSystemUi();
-        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                if (visibility == 0) {
-                    mHandler.postDelayed(mHideRunnable, 2000);
+        if (hideSystemUI) {
+            hideSystemUi();
+            getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    if (visibility == 0) {
+                        mHandler.postDelayed(mHideRunnable, 2000);
+                    }
                 }
-            }
-        });
+            });
+        }
 
         if (keepScreenOn) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -56,7 +68,50 @@ public class MainActivity extends Activity {
         WebView webView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webView.clearCache(true);
         webView.loadUrl(URL);
+
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+//                Log.v(TAG, "touch");
+                if (v.getId() == R.id.webView && event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (lastTouchMs > 0) {
+                        long msDiff = System.currentTimeMillis() - lastTouchMs;
+                        if (msDiff > 1000)
+                            touchCount = 0;
+                        else
+                            touchCount++;
+
+                        Log.v(TAG, "" + touchCount);
+                        if (touchCount == 8) {
+                            showExitDialog();
+                        }
+                    }
+                    lastTouchMs = System.currentTimeMillis();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void showExitDialog() {
+        final EditText input = new EditText(this);
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Hosen runter du nackter Affe")
+                .setView(input)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String s = input.getText().toString();
+                        if (s.equals("ente")) {
+                            finish();
+                        }
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Do nothing.
+            }
+        }).show();
     }
 
     private void hideSystemUi() {
